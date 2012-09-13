84ac9f17be9cfce0d075f54e64507515375186a1
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-09-13 12:49:25 -0700

    Add beginning of models.

---
+++ assets/javascripts/app/models.js
@@ -0,0 +1,12 @@
+GiddyUp.Scorecard = DS.Model.extend({
+  name: DS.attr('string'),
+  project: DS.attr('string'),
+  test_results: DS.hasMany('GiddyUp.TestResult', { key: 'test_result_ids' })
+});
+
+GiddyUp.TestResult = DS.Model.extend({
+  test: DS.attr('string'),
+  platform: DS.attr('string')
+  status: DS.attr('boolean')
+  log_url: DS.attr('string')
+});
