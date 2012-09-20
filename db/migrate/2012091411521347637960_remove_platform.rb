18f94a97942a218a73fc1ce210750740aa04a830
Sean Cribbs <sean@basho.com>
2012-09-20 11:26:02 -0400

    Fix remove platform migration.

--- db/migrate/2012091411521347637960_remove_platform.rb
+++ db/migrate/2012091411521347637960_remove_platform.rb
@@ -1,6 +1,22 @@
 class RemovePlatform < ActiveRecord::Migration
-  def change
+  def up
     drop_table :platforms
     drop_table :platforms_projects
+  end
+
+  def down
+    # Platform
+    create_table :platforms do |t|
+      t.string :name
+      t.integer :position
+    end
+    add_index :platforms, :position
+
+    # Join Platform <-> Project
+    create_table :platforms_projects, :id => false do |t|
+      t.references :platform
+      t.references :project
+    end
+    add_index :platforms_projects, [:project_id, :platform_id]
   end
 end
