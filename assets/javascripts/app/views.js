8cf9cf5695b5cbe48787a41b3e2128731970715e
Sean Cribbs <sean@basho.com>
2012-11-27 10:04:18 -0500

    Redo the prettyDate function based on Rails' helper function.

--- assets/javascripts/app/views.js
+++ assets/javascripts/app/views.js
@@ -1,24 +1,38 @@
 GiddyUp.prettyDate = function(date){
-  // Based on John Resig's prettyDate function for JavaScript &
-  // JQuery.
-  // http://ejohn.org/files/pretty.js
-  var diff = (((new Date()).getTime() - date.getTime()) / 1000),
-      day_diff = Math.floor(diff / 86400);
+  // Based roughly on distance_of_time_in_words from Ruby on Rails,
+  // with ideas from John Resig's "prettyDate" function.
+  var now = (new Date()).getTime(),
+      distance_in_seconds = (now - date.getTime()) / 1000,
+      distance_in_minutes = Math.round(distance_in_seconds / 60.0),
+      distance_in_hours = Math.round(distance_in_minutes / 60.0),
+      distance_in_days = Math.round(distance_in_minutes / 1440.0),
+      distance_in_months = Math.round(distance_in_minutes / 43200.0),
+      distance_in_years = Math.round(distance_in_days / 365);
 
-  if ( isNaN(day_diff) || day_diff < 0 )
-    return;
-
-  return day_diff == 0 && (
-    diff < 60 && "just now" ||
-      diff < 120 && "1 minute ago" ||
-      diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
-      diff < 7200 && "1 hour ago" ||
-      diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
-    day_diff == 1 && "Yesterday" ||
-    day_diff < 7 && day_diff + " days ago" ||
-    day_diff < 42 && Math.ceil( day_diff / 7 ) + " weeks ago" ||
-    day_diff >= 42 && Math.ceil( day_diff / 30 ) + " months ago";
-}
+  if(distance_in_minutes <= 1) {
+    return (distance_in_minutes == 0) ? "just now" : "1 minute ago";
+  } else if(distance_in_minutes >= 2 && distance_in_minutes <= 44) {
+    return distance_in_minutes + " minutes ago";
+  } else if(distance_in_minutes >= 45 && distance_in_minutes <= 89) {
+    return "about 1 hour ago";
+  } else if(distance_in_minutes >= 90 && distance_in_minutes <= 1439) {
+    return "about " + distance_in_hours + " hours ago";
+  } else if(distance_in_minutes >= 1440 && distance_in_minutes <= 2519) {
+    return "1 day ago";
+  } else if(distance_in_minutes >= 2520 && distance_in_minutes <= 43199) {
+    return distance_in_days + " days ago";
+  } else if(distance_in_minutes >= 43200 && distance_in_minutes <= 86399) {
+    return "about 1 month ago";
+  } else if(distance_in_minutes >= 86400 && distance_in_minutes <= 525599) {
+    return distance_in_months + " months ago";
+  } else if(distance_in_months < 13){
+    return "about 1 year ago";
+  } else if(distance_in_years < 2) {
+    return "over 1 year ago";
+  } else {
+    return distance_in_years + " years ago";
+  }
+};
 
 GiddyUp.ApplicationView = Ember.View.extend({
   templateName: 'application'
