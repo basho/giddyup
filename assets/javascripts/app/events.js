1f58fd23e56abd1e7507c3391680e80dde42a9fc
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2013-01-04 17:34:54 -0500

    Desktop notifications for new events.

--- assets/javascripts/app/events.js
+++ assets/javascripts/app/events.js
@@ -6,11 +6,33 @@ GiddyUp.EventProcessor = Ember.Object.extend({
       var parsedResponse = JSON.parse(e.data);
       var testResult = parsedResponse.test_result;
 
+      // Load the new test result into the store.
       GiddyUp.store.load(GiddyUp.TestResult, testResult);
+
+      // Trigger a notification.
+      if (window.webkitNotifications &&
+        window.webkitNotifications.checkPermission() === 0) {
+        window.webkitNotifications.createNotification(
+          'icon.png',
+          'GiddyUp: Test Result Posted!',
+          'Build: ' + testResult.long_version).
+        show();
+      }
     });
   }
 });
 
+/* Enable live event streams if available. */
 if(!!window.EventSource) {
   GiddyUp.eventProcessor = GiddyUp.EventProcessor.create({});
+}
+
+/* Prompt the user once to accept nofications. */
+if (window.webkitNotifications &&
+    window.webkitNotifications.checkPermission() !== 0) {
+  GiddyUp.desktopNotifierBinding = $('a').live('click', function() {
+    window.webkitNotifications.requestPermission(function() {
+      GiddyUp.desktopNotifierBinding.die();
+    });
+  });
 }
