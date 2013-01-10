c9a5db024fb9540c2ca727672dd21c0254ad4733
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2013-01-10 12:13:40 -0500

    New event API.

--- assets/javascripts/app/events.js
+++ assets/javascripts/app/events.js
@@ -2,29 +2,46 @@ GiddyUp.EventProcessor = Ember.Object.extend({
   init: function() {
     this.source = new EventSource('live');
 
-    this.source.addEventListener('test_result', function(e) {
-      var parsedResponse = JSON.parse(e.data);
-      var rawTestResult  = parsedResponse.test_result;
-      var testResult     = GiddyUp.TestResult.loadRawTestResult(
-                            rawTestResult);
+    /** When generic messages are received, generate notifications for
+      * them directly. */
+    this.source.addEventListener('message', function(e) {
+      var parsedEvent = JSON.parse(e.data);
 
-      // Notification handling.
-      //
-      if (window.webkitNotifications &&
-          window.webkitNotifications.checkPermission() === 0) {
-          var message = testResult.get('notification');
-
-          window.webkitNotifications.createNotification(
-            "icon.png", message.title, message.message).
-          show();
-      }
+      GiddyUp.Notification.create({
+        title: parsedEvent.title, message: parsedEvent.message
+      });
     });
+
+    /** When test results are received, force loading of the object by
+      * id, and trigger a notification for the new object */
+    this.source.addEventListener('test_result', function(e) {
+      var parsedEvent  = JSON.parse(e.data);
+      var testResultId = parsedEvent.id;
+      var testResult   = GiddyUp.TestResult.find(testResultId);
+      var notification = testResult.get('notification');
+
+      GiddyUp.Notification.create({
+        title: notification.title, message: notification.message
+      });
+    });
+  }
+});
+
+GiddyUp.Notification = Ember.Object.extend({
+  send: function() {
+    if (window.webkitNotifications &&
+        window.webkitNotifications.checkPermission() === 0) {
+        window.webkitNotifications.createNotification(
+          "icon.png",
+          this.get('title'),
+          this.get('message')).show();
+    }
   }
 });
 
 /* Enable live event streams if available. */
 if(!!window.EventSource) {
-  GiddyUp.eventProcessor = GiddyUp.EventProcessor.create({});
+  GiddyUp.eventProcessor = GiddyUp.EventProcessor.create();
 }
 
 /* Prompt the user once to accept nofications. */
