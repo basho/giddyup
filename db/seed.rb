4b84365beb0a1bf3f6e19b0d632b5beecdcc2263
Sean Cribbs <sean@basho.com>
2013-05-31 12:31:55 -0500

    Parameterize the replication2_pg test. Addresses @metadave comments in #59.

--- db/seed.rb
+++ db/seed.rb
@@ -166,7 +166,15 @@ platforms.each do |p|
     end
   end
 
-  %w{replication2_fsschedule replication2_pg replication2_ssl}.each do |t|
+  %w{replication2_fsschedule replication2_ssl
+     replication2_pg:test_basic_pg_mode_repl13
+     replication2_pg:test_basic_pg_mode_mixed
+     replication2_pg:test_12_pg_mode_repl12
+     replication2_pg:test_12_pg_mode_repl_mixed
+     replication2_pg:test_mixed_pg
+     replication2_pg:test_multiplie_sink_pg
+     replication2_pg:test_bidirectional_pg
+     replication2_pg:test_pg_proxy }.each do |t|
     next if p =~ PLATFORM_SKIPS['1.4']
     create_riak_test t, %w{riak_ee}, 'platform' => p, 'min_version' => '1.4.0'
   end
