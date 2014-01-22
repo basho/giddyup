caebe19b2de2090d355baffda95b5997e7f9bb5d
Sean Cribbs <sean@basho.com>
2014-01-22 12:44:15 -0600

    repl_reduced is only _ee

--- db/migrate/2014012212381390415880_repl_reduced21.rb
+++ db/migrate/2014012212381390415880_repl_reduced21.rb
@@ -1,6 +1,6 @@
 class ReplReduced21 < ActiveRecord::Migration
   def up
-    say_with_time "Moving repl_reduced test to riak 2.1" do
+    say_with_time "Moving repl_reduced test to riak_ee 2.1" do
       Test.where(name: 'repl_reduced').each do |t|
         t.tags['min_version'] = '2.1.0'
         t.save!
