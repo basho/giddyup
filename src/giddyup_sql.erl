943d244021b89ef8c0662c487849c7b2d1ec405e
Sean Cribbs <sean@basho.com>
2014-07-11 16:10:50 -0500

    Add scorecard and test result creation and reorganize a little

--- src/giddyup_sql.erl
+++ src/giddyup_sql.erl
@@ -1,11 +1,20 @@
 %% @doc SQL worker process wrapper and queries/updates
 -module(giddyup_sql).
--compile({inline, [projects_q/0,
-                   project_id_q/0,
-                   suite_q/0,
-                   scorecards_q/0,
+
+%% Inline all the query strings, they're basically constants.
+-compile({inline, [
+                   artifact_i/0,
+                   artifacts_q/0,
                    full_matrix_q/0,
-                   artifacts_q/0]}).
+                   project_id_q/0,
+                   projects_q/0,
+                   scorecard_by_version_q/0,
+                   scorecard_i/0,
+                   scorecard_id_q/0,
+                   scorecards_q/0,
+                   suite_q/0,
+                   test_result_i/0
+                  ]}).
 
 -include_lib("epgsql/include/pgsql.hrl").
 
@@ -18,17 +27,19 @@
          ]).
 
 -export([
+         artifacts/1,
+         create_artifact/3,
+         create_scorecard/2,
+         create_test_result/5,
+         full_matrix/1,
+         next_id/1,
+         project_exists/1,
+         projects/0,
          q/1,
          q/2,
-         projects/0,
-         project_exists/1,
-         suite/3,
-         scorecards/1,
          scorecard_exists/1,
-         %% matrix/1,
-         full_matrix/1,
-         artifacts/1,
-         create_artifact/3
+         scorecards/1,
+         suite/3
         ]).
 
 %%---------------------
@@ -38,11 +49,8 @@ start_link(Args) ->
     %% TODO: make this a proper wrapper, prepare statements
     erlang:apply(pgsql, connect, Args).
 
-extract_column_names(Columns) ->
-    [ Name || #column{name=Name} <- Columns ].
-
 %%---------------------
-%% Query calls
+%% Queries
 %%---------------------
 q(SQL) ->
     ?QUERY(SQL).
@@ -65,14 +73,8 @@ scorecard_exists(ID) ->
             false
     end.
 
-suite(ProjectID, Platform, Version) ->
-    ?QUERY(suite_q(), [ProjectID, Platform, Version]).
-
 scorecards(ProjectID) ->
     ?QUERY(scorecards_q(), [ProjectID]).
-
-%% matrix(ScorecardID) ->
-%%     ?QUERY(matrix_q(), [ScorecardID]).
 
 full_matrix(ScorecardID) ->
     ?QUERY(full_matrix_q(), [ScorecardID]).
@@ -80,20 +82,61 @@ full_matrix(ScorecardID) ->
 artifacts(TestResultID) ->
     ?QUERY(artifacts_q(), [TestResultID]).
 
+%%-----------------------------------------
+%% Riak Test endpoints - suites and results
+%%-----------------------------------------
+
+suite(ProjectID, Platform, Version) ->
+    ?QUERY(suite_q(), [ProjectID, Platform, Version]).
+
+next_id(Table) when is_list(Table) ->
+    SeqName = Table ++ "_id_seq",
+    ?QUERY("SELECT nextval($1)", [SeqName]).
+
 create_artifact(TestResultID, URL, CType) ->
     ?QUERY(artifact_i(), [TestResultID, URL, CType]).
 
+create_test_result(ID, TID, SID, V, St) ->
+    ?QUERY(test_result_i(), [ID, TID, SID, V, St]).
+
+create_scorecard(ProjectID, Version0) ->
+    Version = giddyup_version:parse(Version0),
+    case try_create_scorecard(ProjectID, Version) of
+        {rollback, Result} -> Result;
+        Result -> Result
+    end.
+
+try_create_scorecard(ProjectID, Version) ->
+    transaction(
+      fun(C) ->
+              case pgsql:equery(C, scorecard_by_version_q(), [ProjectID, Version]) of
+                  {ok, _, []} ->
+                      pgsql:equery(C, scorecard_i(), [ProjectID, Version]);
+                  Result ->
+                      Result
+              end
+      end).
+
+transaction(Fun) ->
+    poolboy:transaction(fun(C) -> pgsql:transaction(C, Fun) end).
+
 %%---------------------
-%% Query statement defs
+%% Utilities
 %%---------------------
 
-%% test_result_i() ->
-%%     "INSERT INTO test_results (test_id, scorecard_id, long_version, status, created_at, updated_at) "
-%%     "VALUES ($1, $2, $3, $4, NOW(), NOW()) "
-%%     "RETURNING id".
+extract_column_names(Columns) ->
+    [ Name || #column{name=Name} <- Columns ].
+
+%%---------------------
+%% SQL statement defs
+%%---------------------
+
+test_result_i() ->
+    "INSERT INTO test_results (id, test_id, scorecard_id, long_version, status, created_at, updated_at) "
+    "VALUES ($1, $2, $3, $4, NOW(), NOW())".
 
 artifact_i() ->
-    "INSERT INTO artifacts (test_result_id, url, content_type, created_at, updated_at) "
+    "INSERT INTO artifacts (id, test_result_id, url, content_type, created_at, updated_at) "
     "VALUES ($1, $2, $3, NOW(), NOW()) "
     "RETURNING id".
 
@@ -147,3 +190,9 @@ artifacts_q() ->
     "FROM artifacts "
     "WHERE test_result_id = $1 "
     "ORDER BY created_at DESC".
+
+scorecard_by_version_q() ->
+    "SELECT id FROM scorecards WHERE project_id = $1 AND version = $2".
+
+scorecard_i() ->
+    "INSERT INTO scorecards (project_id, version) VALUES ($1, $2) RETURNING id".
