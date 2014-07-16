5320c3b8cea15721620d4040f7810df1843a3a1d
Sean Cribbs <sean@basho.com>
2014-07-16 17:26:19 -0500

    Add a production config.

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ EXTRA_ARGS="$2"
 if [[ "$MODE" = "dev" ]]; then
     EXTRA_ARGS="$EXTRA_ARGS -s reloader"
 else
-    EXTRA_ARGS="$EXTRA_ARGS -noshell -noinput"
+    EXTRA_ARGS="$EXTRA_ARGS -noshell -noinput -setcookie giddyup"
 fi
 exec erl -pa $PWD/ebin $PWD/deps/*/ebin -sname giddyup -config $MODE.config -s giddyup $EXTRA_ARGS
