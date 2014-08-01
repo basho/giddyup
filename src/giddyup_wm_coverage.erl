f34d3011f5ea5f9aa835c4cf77329359bf7847de
Micah Warren <mwarren@basho.com>
2014-08-01 14:36:49 -0500

    Made platform header link to coverage info.

--- src/giddyup_wm_coverage.erl
+++ src/giddyup_wm_coverage.erl
@@ -1,7 +1,7 @@
 %% @doc After coverage report generated, this resource shoves it out.
 -module(giddyup_wm_coverage).
 
--record(context, {file, test_result}).
+-record(context, {mode, file, test_result}).
 
 -export([
     init/1,
@@ -13,16 +13,35 @@
 -include("giddyup_wm_auth.hrl").
 
 routes() ->
-    [{["test_results", id, "coverage", '*'], ?MODULE, []}].
+    [{["test_results", id, "coverage", '*'], ?MODULE, test_results},
+     {["scorecards", id, platform, "coverage", '*'], ?MODULE, scorecard}].
 
-init(_) ->
-    {ok, #context{}}.
+init(Mode) ->
+    {ok, #context{mode = Mode}}.
 
-resource_exists(RD, Context) ->
+resource_exists(RD, #context{mode = test_results} = Context) ->
     TestResId = wrq:path_info(id, RD),
     TailPath = wrq:disp_path(RD),
     LocalPath = filename:join(["tmp", "coverage", "test_results", TestResId, TailPath]),
     lager:debug("coverage resource - TestResId: ~s, TailPath: ~p; LocalPath: ~p", [TestResId, TailPath, LocalPath]),
+    {Boolean, File} = case filelib:is_dir(LocalPath) of
+        true ->
+            {true, filename:join(LocalPath, "index.html")};
+        false ->
+            case filelib:is_file(LocalPath) of
+                false ->
+                    {false, undefined};
+                true ->
+                    {true, LocalPath}
+            end
+    end,
+    {Boolean, RD, Context#context{file = File}};
+
+resource_exists(RD, #context{mode = scorecard} = Context) ->
+    ScorecardId = wrq:path_info(id, RD),
+    Platform = wrq:path_info(platform, RD),
+    TailPath = wrq:disp_path(RD),
+    LocalPath = filename:join(["tmp", "coverage", "scorecards", ScorecardId, Platform, TailPath]),
     {Boolean, File} = case filelib:is_dir(LocalPath) of
         true ->
             {true, filename:join(LocalPath, "index.html")};
