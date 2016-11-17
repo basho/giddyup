8054ef9e2d4efe8ff7c5cee9d9654838b7b54b4d
Korry Clark <korriganclark@gmail.com>
2016-11-17 12:33:46 -0500

    update test name

--- priv/sql/198-add-orderby-tests.sql
+++ priv/sql/198-add-orderby-tests.sql
@@ -1,7 +1,7 @@
 BEGIN;
 WITH newtests as (INSERT INTO tests (name, platform, backend) VALUES
 
-	   ('ts_cluster_select_desc_SUITE','centos-6-64','eleveldb'),
+       ('ts_cluster_select_desc_SUITE','centos-6-64','eleveldb'),
        ('ts_simple_show_create_table','centos-6-64','eleveldb'),
        ('ts_cluster_updowngrade_order_by_SUITE','centos-6-64','eleveldb'),
        ('ts_cluster_updowngrade_group_by_SUITE','centos-6-64','eleveldb')
@@ -10,5 +10,7 @@ WITH newtests as (INSERT INTO tests (name, platform, backend) VALUES
 INSERT INTO projects_tests (project_id, test_id)
    SELECT projects.id, newtests.id FROM projects, newtests
     WHERE projects.name IN ('riak_ts','riak_ts_ee');
+
+UPDATE tests set name = 'ts_cluster_riak_shell_sql_SUITE' where name = 'ts_cluster_riak_shell_basic_sql';
 
 COMMIT;
