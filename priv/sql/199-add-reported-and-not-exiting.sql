dcf51f75a0d39b6b387afec1a9b2302362f35b99
Korry Clark <korriganclark@gmail.com>
2016-12-01 10:33:13 -0800

    more tests

--- priv/sql/199-add-reported-and-not-exiting.sql
+++ priv/sql/199-add-reported-and-not-exiting.sql
@@ -2,6 +2,9 @@ BEGIN;
 WITH newtests as (INSERT INTO tests (name, platform, backend) VALUES
 
        ('ts_cluster_overload_reported','centos-6-64','eleveldb'),
+       ('ts_simple_blob','centos-6-64','eleveldb'),
+       ('ts_cluster_updowngrade_blob_SUITE','centos-6-64','eleveldb'),
+       ('ts_simple_object_size_limits','centos-6-64','eleveldb'),
        ('ts_simple_select_table_not_existing','centos-6-64','eleveldb')
        RETURNING id)
 
